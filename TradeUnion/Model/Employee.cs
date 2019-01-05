d46c71fd281e81ab4113ccd1d3452dd09f3ea7b2
Andrey <dn6pr0@gmail.com>
2019-01-05 23:11:46 +0200

    Реализованы модели хранимых обектов

---
+++ TradeUnion/Model/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TradeUnion.Model
+{
+    [Serializable]
+    class Employee
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Inn { get; set; }
+        
+        public override bool Equals(object obj)
+        {
+            return obj is Employee && obj.GetHashCode() == GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode();
+        }
+    }
+}
---
+++ TradeUnion/Model/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TradeUnion.Model
+{
+    [Serializable]
+    class Event
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Event && obj.GetHashCode() == GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode();
+        }
+    }
+}
